b5a25d477733531cd9d6513a238075d5d79657b3
Sean Walker <swlkr.rbl@gmail.com>
2015-12-27 12:49:41 -0800

    Add default port 3000

--- src/api/config.clj
+++ src/api/config.clj
@@ -4,7 +4,7 @@
 
 (def connection-string (:database-url @env))
 (def clj-env (:clj-env @env))
-(def port (or (parse-int (:port @env)) 3000))
+(def port (or (parse-int (:port @env) :fallback 3000)))
 
 (def connection {:classname "org.postgresql.Driver"
                  :subprotocol "postgresql"
